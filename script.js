fbc78fa329afc42030284f5e98e73f2a8b93c1e5
Inga Danilova <aminga@yandex.ru>
2020-03-26 04:41:18 +0300

    feat: add singolo-responsive

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function onscroll(event) {
     const links = document.querySelectorAll('#header-menu a');
 
     sect.forEach((el) => {
-        if(el.offsetTop <= curPos+95 && (el.offsetTop + el.offsetHeight) > curPos) {
+        if(el.offsetTop-95 <= curPos && (el.offsetTop + el.offsetHeight-95) > curPos) {
             links.forEach((a) => {
                 a.classList.remove('active');
                 if(el.getAttribute('id') === a.getAttribute('href').substring(1)) {
@@ -19,6 +19,12 @@ function onscroll(event) {
 
 }
 
+
+
+document.querySelector('#toggle-menu').addEventListener('click', function(){
+    document.querySelector('#toggle-menu').classList.toggle('rotate');
+})
+
 // portfolio bordered img
 gallery.addEventListener('click', event => {
     let target = event.target;
@@ -26,7 +32,7 @@ gallery.addEventListener('click', event => {
         gallery.querySelectorAll('img').forEach(item => {
             item.style.boxShadow = "none";
         });
-        event.target.style.boxShadow = "0px 0px 0px 5px #ffffff";
+        event.target.style.boxShadow = "0px 0px 0px 5px #F06C64";
     }
 
 })
@@ -56,57 +62,69 @@ document.getElementById('menu-portfolio').addEventListener('click', (event) => {
     randomImages();
 });
 
+
+
 //slider
-let slideIndex = 0;
-let counter = 0;
 
-showSlides(0);
+function changeSlide (direction) {
+    if(!isEnabled) return;
+    isEnabled = false;
 
-function nextSlide(n) {
-    return showSlides(n);
-};
+    let activeSlide = document.querySelector('.slide.active');
+    let nextSlide = null;
+    let activeSlideX = activeSlide.offsetLeft;
+    let nextSlideX = activeSlide.offsetWidth;
+    let slideSpeed = 34;
 
-function showSlides(n) {
-    let slides = document.querySelectorAll('.slide');
-    slides.forEach(element => {
-        element.style.display = "none"
-    });
-    slideIndex += n;
-    if (slideIndex > slides.length - 1) {
-        slideIndex = 0;
+    if (direction === 'right') {
+        nextSlide = activeSlide.nextElementSibling || activeSlide.parentElement.firstElementChild;
+        nextSlide.style.left = `${activeSlide.clientWidth}px`;
+        activeSlideX = activeSlide.offsetLeft;
+        nextSlideX = activeSlide.offsetWidth;
+        slideSpeed *= -1;
+    } else if (direction === 'left'){
+        nextSlide = activeSlide.previousElementSibling || activeSlide.parentElement.lastElementChild;
+        nextSlide.style.right = `-${activeSlide.clientWidth}px`;
+        activeSlideX = activeSlide.offsetLeft;
+        nextSlideX = -activeSlide.offsetWidth;
     }
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    }
-    slides[slideIndex].style.display = "block";
-    let slider = document.querySelector(".slider");
-    let slide2 = document.querySelector(".slide2");
-    if (slide2.style.display === "block") {
-        slider.classList.add("slider_blue");
-    } else {
-        slider.classList.remove("slider_blue");
-    }
+            
+    let moveSlides = setInterval( () => {
+        activeSlideX += slideSpeed;
+        nextSlideX += slideSpeed;
+        activeSlide.style.left = `${activeSlideX}px`;
+        nextSlide.style.left = `${nextSlideX}px`;
+
+        if (Math.abs(nextSlide.offsetLeft) < Math.abs(slideSpeed)) {
+            activeSlide.style.left = `${activeSlideX - nextSlide.offsetLeft}px`;
+            nextSlide.style.left = `${nextSlideX - nextSlide.offsetLeft}px`;
+            clearInterval(moveSlides);
+            isEnabled = true;
+        }
+    }, 1000/60);
+
+    nextSlide.classList.add('active');
+    activeSlide.classList.remove('active');
 }
 
-document.getElementById('pr-btn').addEventListener('click', (event) => {
-    nextSlide(-1);
-});
 
-document.getElementById('nx-btn').addEventListener('click', (event) => {
-    nextSlide(1);
-});
+
+let isEnabled = true;
+document.querySelector('.prev-button').addEventListener('click', () => changeSlide('left'));
+document.querySelector('.next-button').addEventListener('click', () => changeSlide('right'));
+
 
 
 
 // black screen on/off
-const vertPhone = document.querySelector("#ph-vert");
-const scrVert = document.querySelector("#scr-vert");
+const vertPhone = document.querySelector('#ph-vert');
+const scrVert = document.querySelector('#scr-vert');
 
-const horPhone = document.querySelector("#ph-hor");
-const scrHor = document.querySelector("#scr-hor");
+const horPhone = document.querySelector('#ph-hor');
+const scrHor = document.querySelector('#scr-hor');
 
 vertPhone.addEventListener('click', (event) => {
-    if (scrVert.classList.contains("screen-off")) {
+    if (scrVert.classList.contains('screen-off')) {
         scrVert.classList.remove('screen-off');
     } else {
         scrVert.classList.add('screen-off');
@@ -119,7 +137,7 @@ scrVert.addEventListener('click', (event) => {
 
 
 horPhone.addEventListener('click', (event) => {
-    if (scrHor.classList.contains("screen-off")) {
+    if (scrHor.classList.contains('screen-off')) {
         scrHor.classList.remove('screen-off');
     } else {
         scrHor.classList.add('screen-off');
@@ -132,4 +150,34 @@ scrHor.addEventListener('click', (event) => {
 
 
 
+//form
 
+let form = document.querySelector('#form');
+
+form.addEventListener('submit',  event => {
+    let subject = document.querySelector('#form-subject').value;
+    let describe = document.querySelector('#form-describe').value;
+    let them = document.querySelector('#subject-message');
+    let description = document.querySelector('#describe-message');
+    event.preventDefault();
+    if (form.checkValidity()) {
+        if (subject){
+            them.innerHTML ='<i class="message-title">Subject:</i> '+subject;
+        }
+        else {
+            them.innerHTML='<i class="message-title">No subject</i>';
+        }
+        if (describe){
+            description.innerHTML ='<i class="message-title">Description:</i> '+describe;
+        }
+        else {
+            description.innerHTML='<i class="message-title">No description</i>'; 
+        }
+        document.getElementById('message-block').classList.remove('hidden');
+    }
+});
+
+document.querySelector('#button-close').addEventListener('click', event=> {
+    document.querySelector('#message-block').classList.add('hidden');
+    form.reset();
+});
